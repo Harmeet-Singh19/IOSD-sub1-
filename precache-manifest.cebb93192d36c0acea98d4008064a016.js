d852af0e066e52d601f17b1bdfef37209a598631
Harmeet <savgun2001@gmail.com>
2020-10-06 15:00:58 +0530

    Updates

---
+++ precache-manifest.cebb93192d36c0acea98d4008064a016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbcd2cb3a39a987eebd0b1abd5cf7b7",
+    "url": "/IOSD-sub1-/index.html"
+  },
+  {
+    "revision": "0baf8488f403a108b123",
+    "url": "/IOSD-sub1-/static/css/main.e39edf1a.chunk.css"
+  },
+  {
+    "revision": "006044542c989263dae4",
+    "url": "/IOSD-sub1-/static/js/2.62178fb6.chunk.js"
+  },
+  {
+    "revision": "85c1ecd325742a4c0551d2653970c6fb",
+    "url": "/IOSD-sub1-/static/js/2.62178fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0baf8488f403a108b123",
+    "url": "/IOSD-sub1-/static/js/main.79ebe613.chunk.js"
+  },
+  {
+    "revision": "68974b411e7e05ad43d9",
+    "url": "/IOSD-sub1-/static/js/runtime-main.168056d9.js"
+  }
+]);
